15997c98129e6fc54185c16288273c5189614935
gamename <tennis.n.smith@gmail.com>
2024-12-08 18:48:21 -0600

    refactor to simplify

--- nvg_main.c
+++ nvg_main.c
@@ -25,18 +25,16 @@
 #define NANOVG_GLES2_IMPLEMENTATION
 #include "nanovg_gl.h"
 #include "nanovg_gl_utils.h"
-//#include "perf.h"
+// #include "perf.h"
 #include "nvg_main.h"
 #include "fonts.h"
 
-
-void errorcb(int error, const char* desc)
+void errorcb(int error, const char *desc)
 {
 	printf("GLFW error %d: %s\n", error, desc);
 }
 
-
-static void key(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
 	NVG_NOTUSED(scancode);
 	NVG_NOTUSED(mods);
@@ -44,18 +42,21 @@ static void key(GLFWwindow* window, int key, int scancode, int action, int mods)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext *), int init_window_width, int init_window_height)
+int nvg_main(void (*drawFrame)(NVGcontext *, int, int), void (*nvgInit)(NVGcontext *), int init_window_width, int init_window_height)
 {
-	GLFWwindow* window;
-	NVGcontext* vg = NULL;
-	//PerfGraph fps;
+	GLFWwindow *window;
+	NVGcontext *vg = NULL;
+	// PerfGraph fps;
 
-	if (!glfwInit()) {
+	glfwSetErrorCallback(errorcb);
+
+	if (!glfwInit())
+	{
 		printf("Failed to init GLFW.");
 		return -1;
 	}
 
-	//initGraph(&fps, GRAPH_RENDER_FPS, "Frame Time");
+	// initGraph(&fps, GRAPH_RENDER_FPS, "Frame Time");
 
 	glfwSetErrorCallback(errorcb);
 
@@ -63,17 +64,18 @@ int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-        if(init_window_width == 0 || init_window_height == 0)
-        {
-            GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-            const GLFWvidmode* vidMode = glfwGetVideoMode(monitor);
-            window = glfwCreateWindow(vidMode->width, vidMode->height, "PiClock", monitor, NULL);
-        }
-        else
-        {
-            window = glfwCreateWindow(init_window_width, init_window_height, "PiClock", NULL, NULL);
-        }
-	if (!window) {
+	if (init_window_width == 0 || init_window_height == 0)
+	{
+		GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode *vidMode = glfwGetVideoMode(monitor);
+		window = glfwCreateWindow(vidMode->width, vidMode->height, "PiClock", monitor, NULL);
+	}
+	else
+	{
+		window = glfwCreateWindow(init_window_width, init_window_height, "PiClock", NULL, NULL);
+	}
+	if (!window)
+	{
 		glfwTerminate();
 		return -1;
 	}
@@ -83,7 +85,8 @@ int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext 
 	glfwMakeContextCurrent(window);
 
 	vg = nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
-	if (vg == NULL) {
+	if (vg == NULL)
+	{
 		printf("Could not init nanovg.\n");
 		return -1;
 	}
@@ -92,8 +95,8 @@ int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext 
 
 	glfwSetTime(0);
 
-        if(nvgInit != 0)
-            nvgInit(vg);
+	if (nvgInit != 0)
+		nvgInit(vg);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -110,8 +113,8 @@ int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext 
 
 		// Update and render
 		glViewport(0, 0, fbWidth, fbHeight);
-		glClearColor(0,0,0,0);
-		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -120,7 +123,7 @@ int nvg_main(void (*drawFrame)(NVGcontext*,int,int), void (*nvgInit)(NVGcontext 
 
 		nvgBeginFrame(vg, winWidth, winHeight, pxRatio);
 
-		drawFrame(vg, winWidth,winHeight);
+		drawFrame(vg, winWidth, winHeight);
 
 		nvgEndFrame(vg);
 
--- piclock.cpp
+++ piclock.cpp
@@ -37,9 +37,7 @@
 #include "regionstate.h"
 #include "imagescaling.h"
 #include "overallstate.h"
-#include "analogueclock.h"
 #include "gpio.h"
-
 
 #define FPS 25
 #define FRAMES 0
@@ -47,7 +45,6 @@
 namespace po = boost::program_options;
 
 static OverallState globalState;
-
 
 int GPI_MODE = 0;
 int GPIO_TYPE = 0;
@@ -59,13 +56,10 @@ std::string clean_exit_file("/tmp/piclock_clean_exit");
 void DrawFrame(NVGcontext *vg, int iwidth, int iheight);
 void NvgInit(NVGcontext *vg);
 
-
-
-
-void * ntp_check_thread(void * arg)
+void *ntp_check_thread(void *arg)
 {
-	ntpstate_t * data = (ntpstate_t *)arg;
-	while(bRunning)
+	ntpstate_t *data = (ntpstate_t *)arg;
+	while (bRunning)
 	{
 		get_ntp_state(data);
 		sleep(1);
@@ -73,31 +67,13 @@ void * ntp_check_thread(void * arg)
 	return NULL;
 }
 
-
-
-
-
-
-void read_settings(const std::string & filename,
-		   po::variables_map& vm)
+void read_settings(const std::string &filename, po::variables_map &vm)
 {
 	po::options_description desc("Options");
-	desc.add_options()
-		("init_window_width", po::value<int>(&init_window_width)->default_value(0), "Initial window width, specifying 0 gives fullscreen mode")
-		("init_window_height", po::value<int>(&init_window_height)->default_value(0), "Initial window height, specifying 0 gives fullscreen mode")
-		("gpio_mode", po::value<int>(&GPIO_TYPE)->default_value(0), "GPIO Type, 0=PiFace Digital, 1=Raspberry Pi (not yet implemented)")
-		("gpio_pulls", po::value<std::string>(&GPIO_PULLS)->default_value("UUUUUUUU"), "GPI Pull Up/Down/Off status")
-		("tally_mode", po::value<int>(&GPI_MODE)->default_value(0), "Tally Mode, 0=disabled, 1=GPI/O, 2=TCP/IP, 3=TCP/IP with GPIO status passed back to controller")
-		("tally_remote_host", po::value<std::vector<std::string>>(&tally_hosts), "Remote tally host, may be specified multiple times for multiple connections")
-		("tally_remote_port", po::value<std::string>(&TALLY_SERVICE)->default_value("6254"), "Port (or service) to connect to on (default 6254)")
-		("tally_shared_secret", po::value<std::string>(&TALLY_SECRET)->default_value("SharedSecretGoesHere"), "Shared Secret (password) for connecting to tally service")
-		("clean_exit_file", po::value<std::string>(&clean_exit_file)->default_value("/tmp/piclock_clean_exit"), "Flag file created to indicate a clean exit (from keyboard request)")
-	;
-	
-	std::ifstream settings_file(filename.c_str());
-	
-	vm = po::variables_map();
+	desc.add_options()("init_window_width", po::value<int>(&init_window_width)->default_value(0), "Initial window width, specifying 0 gives fullscreen mode")("init_window_height", po::value<int>(&init_window_height)->default_value(0), "Initial window height, specifying 0 gives fullscreen mode")("gpio_mode", po::value<int>(&GPIO_TYPE)->default_value(0), "GPIO Type, 0=PiFace Digital, 1=Raspberry Pi (not yet implemented)")("gpio_pulls", po::value<std::string>(&GPIO_PULLS)->default_value("UUUUUUUU"), "GPI Pull Up/Down/Off status")("tally_mode", po::value<int>(&GPI_MODE)->default_value(0), "Tally Mode, 0=disabled, 1=GPI/O, 2=TCP/IP, 3=TCP/IP with GPIO status passed back to controller")("tally_remote_host", po::value<std::vector<std::string>>(&tally_hosts), "Remote tally host, may be specified multiple times for multiple connections")("tally_remote_port", po::value<std::string>(&TALLY_SERVICE)->default_value("6254"), "Port (or service) to connect to on (default 6254)")("tally_shared_secret", po::value<std::string>(&TALLY_SECRET)->default_value("SharedSecretGoesHere"), "Shared Secret (password) for connecting to tally service")("clean_exit_file", po::value<std::string>(&clean_exit_file)->default_value("/tmp/piclock_clean_exit"), "Flag file created to indicate a clean exit (from keyboard request)");
 
+	std::ifstream settings_file(filename.c_str());
+	vm = po::variables_map();
 	po::store(po::parse_config_file(settings_file, desc), vm);
 	po::notify(vm);
 }
@@ -107,45 +83,34 @@ void cleanup()
 	bRunning = false;
 	ResizeQueue::Abort();
 
-	//If there's a clean exit trigger file, create it
-	if(!clean_exit_file.empty())
+	if (!clean_exit_file.empty())
 		open(clean_exit_file.c_str(), O_CREAT, 00666);
 	exit(0);
 }
 
 void KeyCallback(unsigned char key, int x, int y)
 {
-	if(key == 'q')
+	if (key == 'q')
 	{
 		cleanup();
 	}
 }
 
-
-/* All variables which used to be local to main(), which we've had
-   to make global now that drawing is done via a callback
- */
 static struct timeval tval;
 static ntpstate_t ntp_state_data;
-static int vrate;
-static int hrate;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	po::variables_map vm;
 
 	std::string configFile = "/etc/piclock.cfg";
 
-	
-	if(argc > 1)
+	if (argc > 1)
 		configFile = argv[1];
 	read_settings(configFile, vm);
 
-	gettimeofday(&tval, NULL);//Just for handle_clock_messages on first pass
+	gettimeofday(&tval, NULL);
 
-
-	srand(time(NULL));
-	vrate = 1800 + (((VGfloat)rand())*1800.0f)/RAND_MAX;
-	hrate = 1800 + (((VGfloat)rand())*1800.0f)/RAND_MAX;
 	init_ntp_state();
 	get_ntp_state(&ntp_state_data);
 	pthread_t ntp_thread;
@@ -156,191 +121,84 @@ int main(int argc, char *argv[]) {
 	struct sched_param resize_param;
 	resize_param.sched_priority = sched_get_priority_min(SCHED_IDLE);
 	pthread_setschedparam(resize_thread.native_handle(), SCHED_IDLE, &resize_param);
-	if(GPI_MODE & 1)
-                gpio_init(GPIO_TYPE, GPIO_PULLS);
-	if(GPI_MODE & 2)
+	if (GPI_MODE & 1)
+		gpio_init(GPIO_TYPE, GPIO_PULLS);
+	if (GPI_MODE & 2)
 		create_tcp_threads();
 
 	nvg_main(DrawFrame, NvgInit, init_window_width, init_window_height);
 
-	//Shouldn't ever get here, but no harm in cleaning up anyway
 	cleanup();
 }
 
 void NvgInit(NVGcontext *vg)
 {
-    //Called at NVG startup before loop
-
-    //Mostly just loads default fonts
-    globalState.NvgInit(vg);
+	globalState.NvgInit(vg);
 }
+
 void DrawFrame(NVGcontext *vg, int iwidth, int iheight)
 {
-	static VGfloat offset = iheight*.05f;
+	static struct tm tm_local;
 
-	static time_t tm_last_comms_good = 0;
-
-	static VGfloat h_offset_pos = 0;
-	static VGfloat v_offset_pos = 0;
-
-	static long prev_sec = 0;
-
-	static struct tm tm_local, tm_utc;
-
-	static bool bRecalcTextsNext = true;
-
-	RotateTextClipCache();
-
-	if(globalState.Regions.empty())
-		//Add 1 region, for when we have no TCP connection, should probably only happen on first pass
-		globalState.Regions[0] = std::make_shared<RegionState>();
-	//Handle any queued messages
-	std::queue<std::shared_ptr<ClockMsg> > newMsgs;
-	msgQueue.Get(newMsgs);
-	bool bRecalcTexts = globalState.HandleClockMessages(vg, newMsgs, tval) || bRecalcTextsNext;
-	bRecalcTextsNext = false;
-	
-	bool bDigitalClockPrefix = RegionState::DigitalClockPrefix(globalState.Regions);
-
-	//nvgTransform(vg, 1,iheight,0,-1,0,0);
-	//Start(iwidth, iheight);					// Start the picture
-	VGfloat display_width = iwidth;
-	VGfloat display_height = iheight;
-	if(globalState.ScreenSaver())
-	{
-		display_width -= offset;
-		display_height -= offset;
-	}
-	if(globalState.RotationReqd(display_width,display_height))
-	{
-		Rotate(vg,90);
-		//After rotating our output has now disappeared down below our co-ordinate space, so translate our co-ordinate space down to find it...
-		nvgTranslate(vg,0,-iwidth);
-		std::swap(display_width,display_height);
-	}
-	
-	//Screen saver offsets...
-	if(globalState.ScreenSaver())
-	{
-		if(tval.tv_sec != prev_sec)
-		{
-			prev_sec =  tval.tv_sec;
-			h_offset_pos = abs(prev_sec%(hrate*2) - hrate)*offset/hrate;
-			v_offset_pos = abs(prev_sec%(vrate*2) - vrate)*offset/vrate;
-		}
-		nvgTranslate(vg, h_offset_pos, v_offset_pos);
-	}
-	for(const auto & region : globalState.Regions)
-	{
-		auto &RS = *region.second;
-		VGfloat inner_height = display_height * RS.height();
-		VGfloat inner_width = display_width * RS.width();
-
-		bRecalcTexts = RS.RecalcDimensions(vg, globalState, tm_utc, tm_local, inner_width, inner_height, display_width, display_height, region.first == 0, bDigitalClockPrefix) || bRecalcTexts;
-	}
-	if(bRecalcTexts)
-	{
-		globalState.TextSizes.clear();
-		globalState.LabelSizes.clear();
-		for(const auto & region : globalState.Regions)
-		{
-			auto &RS = *region.second;
-			RS.SetDefaultZone("R" + std::to_string(region.first));
-			RS.RecalcTexts(vg, globalState, tval);
-		}
-	}
+	// Update the current time
 	gettimeofday(&tval, NULL);
 	localtime_r(&tval.tv_sec, &tm_local);
-	gmtime_r(&tval.tv_sec, &tm_utc);
-	for(const auto & region : globalState.Regions)
-	{
-		std::string profName;
-		std::shared_ptr<RegionState> pRS = region.second;
 
-		VGfloat return_x = display_width *pRS->x();
-		VGfloat return_y = display_height *(pRS->top_y());
-		nvgSave(vg);//Save current translation to stack to be restored later
-		nvgTranslate(vg, return_x, return_y);
+	// Set the background color
+	nvgBeginPath(vg);
+	nvgRect(vg, 0, 0, iwidth, iheight);
+	nvgFillColor(vg, nvgRGB(0, 0, 0)); // Black background
+	nvgFill(vg);
 
-		nvgFillColor(vg, colWhite);					// white text
-		nvgStrokeColor(vg, colWhite);				// White strokes
-		nvgStrokeWidth(vg, 0);//Don't draw strokes until we've moved onto the analogue clock
+	// Set the text color and font for the day and date
+	nvgFontFace(vg, globalState.FontDate().c_str());
+	nvgFillColor(vg, nvgRGB(255, 255, 255)); // White text
 
-		//Write out the text
-		DisplayBox db;
-		int pointSize;
-		std::string prefix;
-		if(pRS->DigitalLocal(db, pointSize, prefix))
-		{
-			std::string time_str = pRS->FormatTime(tm_local, tval.tv_usec);
-			if(bDigitalClockPrefix)
-				time_str = prefix + time_str;
-			db.TextMidBottom(vg, time_str, globalState.FontDigital(), pointSize);
-		}
-		if(pRS->DigitalUTC(db, pointSize, prefix))
-		{
-			std::string time_str = pRS->FormatTime(tm_utc, tval.tv_usec);
-			if(bDigitalClockPrefix)
-				time_str = prefix + time_str;
-			db.TextMidBottom(vg, time_str, globalState.FontDigital(), pointSize);
-		}
-		bool bLocal;
-		if(pRS->Date(db, bLocal, pointSize))
-		{
-			db.TextMidBottom(vg, pRS->FormatDate(bLocal? tm_local: tm_utc),
-						globalState.FontDate(), pointSize);
-		}
+	// Format the day and date string (abbreviated format, no year)
+	char dateStr[64];
+	strftime(dateStr, sizeof(dateStr), "%a, %b %d", &tm_local); // Abbreviated day and month
 
-		if(GPI_MODE == 1)
-		{
-			uint16_t gpis = read_gpi();
-			if(pRS->TD.nRows != 2)
-				bRecalcTextsNext = true;
-			pRS->TD.nRows = 2;
-			pRS->TD.nCols_default = 1;
-			pRS->TD.nCols.clear();
-			uint8_t colour_weight = (gpis & 1) ? 50:255;
-			uint8_t fill_weight = (gpis & 1)? 50:255;
-			pRS->TD.displays[0][0] = std::make_shared<SimpleTallyState>(
-				    TallyColour(fill_weight, fill_weight, fill_weight),
-				    TallyColour(colour_weight, colour_weight*0.55,0),
-				    "Mic Live");
-			colour_weight = (gpis & 2) ? 50:255;
-			fill_weight = (gpis & 2)? 50:255;
-			pRS->TD.displays[1][0] = std::make_shared<SimpleTallyState>(
-				    TallyColour(fill_weight, fill_weight, fill_weight),
-				    TallyColour(colour_weight,0,0),
-				    "On Air");
-		}
-                else if(GPI_MODE == 3)
-                {
-			update_tcp_gpis(read_gpi());
-                }
+	// Determine font size for the day and date
+	float dateFontSize = iheight * 0.15; // Smaller font size for day/date
+	nvgFontSize(vg, dateFontSize);
 
-		if(pRS->HasStatusBox())
-		{
-			bool bAnyComms = pRS->DrawStatusArea(vg, ntp_state_data.status, tval.tv_sec % 2, (GPI_MODE & 2)? tally_hosts.size() : 0, bComms, mac_address, globalState.FontStatus());
-			if(bAnyComms)
-			{
-				tm_last_comms_good = tval.tv_sec;
-			}
-			//Stop showing stuff after 5 seconds of comms failed...
-			else if(tm_last_comms_good < tval.tv_sec - 5)
-			{
-				for(auto & reg: globalState.Regions)
-				{
-					reg.second->TD.clear();
-				}
-			}
-		}
-		//Draw Tally Displays
-		if(GPI_MODE)
-		{
-			pRS->DrawTallies(vg, globalState, tval);
-		}
-		//Right, now start drawing the clock if it exists in our region
-		pRS->DrawAnalogueClock(vg, tm_local, tm_utc, tval.tv_usec, globalState.FontHours(), globalState.Images);
-		//Translate back to the origin...
-		nvgRestore(vg);
-	}
+	// Measure text bounds for the day and date
+	float dateBounds[4];
+	nvgTextBounds(vg, 0, 0, dateStr, NULL, dateBounds);
+
+	float dateTextWidth = dateBounds[2] - dateBounds[0];
+	float dateTextHeight = dateBounds[3] - dateBounds[1];
+	float dateX = (iwidth - dateTextWidth) / 2.0f;
+
+	// Position the date in the upper half of the screen
+	float dateY = (iheight / 4.0f) + (dateTextHeight / 2.0f);
+
+	// Render the day and date
+	nvgText(vg, dateX, dateY, dateStr, NULL);
+
+	// Set the text color and font for the time
+	nvgFontFace(vg, globalState.FontDigital().c_str());
+	nvgFillColor(vg, nvgRGB(255, 255, 255)); // White text
+
+	// Format the local time string (hours and minutes only)
+	char timeStr[64];
+	strftime(timeStr, sizeof(timeStr), "%H:%M", &tm_local);
+
+	// Determine font size for the time (increased by 20%)
+	float timeFontSize = iheight * 0.48; // Larger font size for time
+	nvgFontSize(vg, timeFontSize);
+
+	// Measure text bounds for the time
+	float timeBounds[4];
+	nvgTextBounds(vg, 0, 0, timeStr, NULL, timeBounds);
+
+	float timeTextWidth = timeBounds[2] - timeBounds[0];
+	float timeTextHeight = timeBounds[3] - timeBounds[1];
+	float timeX = (iwidth - timeTextWidth) / 2.0f;
+
+	// Position the time in the center of the screen
+	float timeY = (iheight / 2.0f) + (timeTextHeight / 2.0f);
+
+	// Render the time
+	nvgText(vg, timeX, timeY, timeStr, NULL);
 }
